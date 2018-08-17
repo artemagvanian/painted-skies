f9062d7cb84c1bad4379f9b94a08736f50839896
artemka <avanglion59@gmail.com>
2018-08-17 09:06:56 +0300

    Create full backend for image processing

--- backend/syndicationframework/mindmapper/views.py
+++ backend/syndicationframework/mindmapper/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 import json
@@ -9,6 +9,7 @@ import io
 from PIL import Image
 
 from django.views.decorators.csrf import csrf_exempt
+import pytesseract
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -21,7 +22,7 @@ class ProcessView(View):
                         canvas['backgroundImage']['src'].split(",")[1])))
 
         image = image.resize((int(image.width * canvas['backgroundImage']['scaleX']),
-                     int(image.height * canvas['backgroundImage']['scaleY'])))
+                              int(image.height * canvas['backgroundImage']['scaleY'])))
 
         rectangles = list(map(lambda x: {
             'left': x['left'],
@@ -37,14 +38,70 @@ class ProcessView(View):
         canvas = json.loads(request.POST['canvas'])
         image, rectangles = self.process_canvas(canvas)
 
+        color_to_level = {
+            'rgba(255,0,0,.5)': 1,
+            'rgba(0,255,0,.5)': 2,
+            'rgba(0,0,255,.5)': 3
+        }
+
         regions = list(
-            map(lambda x: image.crop((
-                x['left'],
-                x['top'],
-                x['left'] + x['width'],
-                x['top'] + x['height'])), rectangles))
+            map(lambda x: {
+                'image': image.crop((
+                    x['left'],
+                    x['top'],
+                    x['left'] + x['width'],
+                    x['top'] + x['height'])),
+                'level': color_to_level[x['fill']],
+                'color': x['fill']
+            }, rectangles))
 
-        for n, d in enumerate(regions):
-            d.save(f'{n}.png')
+        nodes = []
+        edges = []
+        stack = []
 
-        return HttpResponse(status=200)
+        for n, i in enumerate(regions):
+            # if i['level'] == 1:
+            #     color = 'red'
+            # elif i['level'] == 2:
+            #     color = 'green'
+            # else:
+            #     color = 'blue'
+            # dot.node(str(n), i.data, color=color, style='filled')
+            nodes.append({
+                'id': n,
+                'label': pytesseract.image_to_string(i['image'], lang='eng'),
+                'color': i['color']
+            })
+
+            if n == 0:
+                stack.append(n)
+            else:
+                if i['level'] > regions[n - 1]['level']:
+                    # dot.edge(n, stack[len(stack) - 1])
+                    edges.append({
+                        'from': n,
+                        'to': stack[len(stack) - 1]
+                    })
+                    stack.append(n)
+                elif i['level'] == regions[n - 1]['level']:
+                    stack.pop()
+                    # dot.edge(n, stack[len(stack) - 1])
+                    edges.append({
+                        'from': n,
+                        'to': stack[len(stack) - 1]
+                    })
+                    stack.append(n)
+                elif i['level'] < regions[n - 1]['level']:
+                    stack.pop()
+                    stack.pop()
+                    # dot.edge(n, stack[len(stack) - 1])
+                    edges.append({
+                        'from': n,
+                        'to': stack[len(stack) - 1]
+                    })
+                    stack.append(n)
+
+        return JsonResponse({
+            'edges': edges,
+            'nodes': nodes
+        })
