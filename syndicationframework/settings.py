4f44d14ad1e077b9d787fff30659db39f9d83e56
artemka <avanglion59@gmail.com>
2018-08-28 14:58:32 +0300

    Add language support

--- syndicationframework/settings.py
+++ syndicationframework/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist/static'),
