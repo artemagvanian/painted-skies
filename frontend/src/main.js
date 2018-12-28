cd5b451c8ed0dcf674d4c6cd8d6ad66439f321a1
Artemka <avanglion59@gmail.com>
2018-12-28 09:43:46 +0200

    Add analytics env

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,64 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false;
 
+import BootstrapVue from 'bootstrap-vue'
+import VueSession from 'vue-session'
+
+Vue.use(VueSession);
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+    faArrowsAlt,
+    faPen,
+    faHandPointer,
+    faTimes,
+    faSearchPlus,
+    faSearchMinus,
+    faArrowRight,
+    faFilePdf,
+    faFileImage,
+    faBook
+} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(
+    faArrowsAlt,
+    faPen,
+    faHandPointer,
+    faTimes,
+    faSearchPlus,
+    faSearchMinus,
+    faArrowRight,
+    faFilePdf,
+    faFileImage,
+    faBook
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+import * as Sentry from '@sentry/browser'
+import VueAnalytics from 'vue-analytics'
+
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+        dsn: process.env.VUE_APP_SENTRY_PUBLIC_DSN,
+        integrations: [new Sentry.Integrations.Vue({Vue})]
+    });
+
+    Vue.use(VueAnalytics, {
+        id: process.env.VUE_APP_GOOGLE_ANALYTICS,
+        router
+    });
+}
+
 new Vue({
+    router,
     render: h => h(App)
 }).$mount('#app');
---
+++ frontend/src/router.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import MindmapViewer from './components/MindmapViewer'
+import PdfUploader from './components/PdfUploader'
+import ImageUploader from './components/ImageUploader'
+import CanvasSelector from './components/CanvasSelector'
+import Menu from './components/Menu'
+import MindmapList from './components/MindmapList'
+import Login from './components/Login'
+import Logout from './components/Logout'
+
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/mindmap-viewer', component: MindmapViewer, props: true, name: 'mindmap'},
+    {path: '/canvas-selector', component: CanvasSelector, props: true, name: 'canvas'},
+    {path: '/', component: Menu, name: 'menu'},
+    {path: '/pdf-uploader', component: PdfUploader, name: 'pdf'},
+    {path: '/image-uploader', component: ImageUploader, name: 'image'},
+    {path: '/list', component: MindmapList, name: 'list'},
+    {path: '/login', component: Login, name: 'login'},
+    {path: '/logout', component: Logout, name: 'logout'},
+];
+
+const router = new VueRouter({
+    routes
+});
+
+export default router
