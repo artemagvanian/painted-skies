691a4cf1f2d0e13f994e9d175312b372bd6de961
Artemka <avanglion59@gmail.com>
2019-03-09 12:09:40 +0200

    Change admin interface

--- backend/mindmapper/admin.py
+++ backend/mindmapper/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Mindmap
 
-# Register your models here.
 
-admin.site.register(Mindmap)
+class MindmapAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'created_at')
+    list_filter = ('owner',)
+    search_fields = ('title',)
+
+
+admin.site.register(Mindmap, MindmapAdmin)
